2c30418062062c1b347aae7a0ecd8bdcb45433b3
Tomas Sans <tomsan@gmail.com>
2022-11-17 09:19:26 +0100

    Canvis

--- MySQL/Entrega 2.1 - Estructures/N1E2_Pizzeria/consultes_pizzeria.sql
+++ MySQL/Entrega 2.1 - Estructures/N1E2_Pizzeria/consultes_pizzeria.sql
@@ -1,4 +1,4 @@
--- Active: 1666904461314@@127.0.0.1@3306@pizzeria
+-- Active: 1648628258354@@127.0.0.1@3306@pizzeria
 
 -- 1. Llista quants productes de tipus “Begudes”. s'han venut en una determinada localitat.
 SELECT Count(p.`ID_Producte`) as Nombre_Productes_Venuts, b.`Localitat` FROM producte p INNER JOIN tipusproducte tp ON p.`ID_TipusProducte` = tp.`ID_TipusProducte` INNER JOIN detallcomanda dc ON dc.`ID_Producte` = p.`ID_Producte` INNER JOIN comanda c ON c.`ID_Comanda` = dc.`ID_Comanda`INNER JOIN botiga b ON b.`ID_Botiga` = c.`ID_Botiga` WHERE tp.`Nom` = 'Beguda' AND b.`Localitat` = 'Barcelona' GROUP BY b.`Localitat`;

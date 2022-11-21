1b37b93cee8e9f0224d99e652eecd52cc4c4a9f7
Tomas Sans <tomsan@gmail.com>
2022-11-21 11:41:58 +0100

    Arreglos i Canvis

--- MySQL/Entrega 2.1 - Estructures/N2E1_Youtube/youtube.sql
+++ MySQL/Entrega 2.1 - Estructures/N2E1_Youtube/youtube.sql
@@ -1,10 +1,6 @@
 DROP DATABASE IF EXISTS YOUTUBE;
 
-CREATE DATABASE
-    IF NOT EXISTS `youtube`
-    /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */
-    /*!80016 DEFAULT ENCRYPTION='N' */
-;
+CREATE DATABASE IF NOT EXISTS `youtube`;
 
 USE `youtube`;
 
@@ -223,8 +219,7 @@ VALUES (
         1
     );
 
-INSERT INTO
-    `historial_videos`
+INSERT INTO `historial_videos`
 VALUES (
         1,
         1,
